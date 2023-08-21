c1295d365bd8433513bd465d1e4a5a51fcd561e0
kingmugi <kingmugisam@gmail.com>
2023-08-21 13:58:10 +0800

    增加動畫特效

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
-console.log("Hello world!");
+AOS.init({
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+});
